08e6c12ba9af85f9239ec07ae9467bb2b160421b
Bende Hajdu <outdoorsport85@gmail.com>
2018-01-29 15:22:18 +0100

    Company input fields are in the html

--- data_manager.py
+++ data_manager.py
@@ -17,10 +17,10 @@ def add_to_individuals(cursor, new_booking):
 @connection.connection_handler
 def add_to_company(cursor, new_booking):
     cursor.execute("""
-                        INSERT INTO company (booking_id, company_name, email, phone_number,
+                        INSERT INTO company (booking_id, company_name, comp_email, comp_phone_number,
                         booked_tables, zip_code, city, street_address, street_type, street_num,
                         floor_door, vat_number, date)
-                        VALUES (%(booking_id)s, %(name)s, %(email)s, %(phone_number)s,
+                        VALUES (%(booking_id)s, %(name)s, %(comp_email)s, %(comp_phone_number)s,
                         %(booked_tables)s, %(zip_code)s, %(city)s, %(street_address)s, %(street_type)s,
                         %(street_num)s, %(floor_door)s, %(vat_number)s, now());
                         """, new_booking)
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import data_manager
 
 app = Flask(__name__)
@@ -9,14 +9,14 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/booking', methods=['GET', 'POST'])
+@app.route('/booking', methods=['POST'])
 def booking():
     new_booking = data_manager.booking_code_generator(request.form.to_dict())
     if "comp_name" in new_booking:
         data_manager.add_to_company(new_booking)
     else:
         data_manager.add_to_individuals(new_booking)
-    return render_template('index.html')
+    return redirect('/')
 
 
 if __name__ == '__main__':
