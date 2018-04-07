1bf6b016f46e583ba27fb155d9c3a812523f7b9d
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-04-07 17:52:28 +0200

    Put all public space to the dropdown in the booking and the modify booking as well

--- util.py
+++ util.py
@@ -11,3 +11,9 @@ def booking_id_added_to_new_booking(new_booking):
     booking_id = generate_code(6)
     new_booking.update({"booking_id": booking_id})
     return new_booking
+
+
+def read_public_spaces_from_file():
+    with open("data/public_space_names.txt") as file:
+        names = file.read().splitlines()
+    return names
