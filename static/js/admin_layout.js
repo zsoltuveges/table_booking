362f6861047f14c47b20ecf7d9c465f00cbc3ecb
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-05-19 15:43:20 +0200

    Fix the bug where the non visibility notification dropdown block the click to the sorting of booked tables

--- static/js/admin_layout.js
+++ static/js/admin_layout.js
@@ -9,9 +9,16 @@ admin_layout = {
         notificationDiv.addEventListener('click', function () {
             let notificationDropdown = document.getElementById("notification_dropdown");
             let style = window.getComputedStyle(notificationDropdown);
+            if (style.getPropertyValue("display") === "none") {
+                notificationDropdown.style.display = "block";
+            } else {
+                const timer = ms => new Promise(resolve => setTimeout(resolve, ms));
+                timer(500).then(() => {
+                    notificationDropdown.style.display = "none";
+                });
+            }
             style.getPropertyValue("opacity") === "1" ? notificationDropdown.style.opacity = "0" : notificationDropdown.style.opacity = "1";
             style.getPropertyValue("top") === "30px" ? notificationDropdown.style.top = "55px" : notificationDropdown.style.top = "30px";
-            console.log("mouseover");
         });
     }
 
