a80fea9ddf0c234ff7ae3f2a60a297d0cd9bca5c
Zsolt Üveges <zsolt.uveges93@gmail.com>
2018-05-31 15:31:51 +0200

    Admin can click anywhere on the page to closing the notification dropdown

--- static/js/admin_layout.js
+++ static/js/admin_layout.js
@@ -2,6 +2,7 @@ admin_layout = {
 
     init: function () {
         this.notificationDivEventListener();
+        this.eventListenerToDocument();
     },
 
     notificationDivEventListener: function () {
@@ -13,13 +14,38 @@ admin_layout = {
                 notificationDropdown.style.display = "block";
             } else {
                 const timer = ms => new Promise(resolve => setTimeout(resolve, ms));
-                timer(500).then(() => {
+                timer(200).then(() => {
                     notificationDropdown.style.display = "none";
                 });
             }
             style.getPropertyValue("opacity") === "1" ? notificationDropdown.style.opacity = "0" : notificationDropdown.style.opacity = "1";
             style.getPropertyValue("top") === "30px" ? notificationDropdown.style.top = "55px" : notificationDropdown.style.top = "30px";
         });
+    },
+
+    eventListenerToDocument: function () {
+        document.addEventListener("click", function () {
+            let target = event.target;
+            let notificationSign = document.getElementById("notification_dropdown_title");
+            let notificationBell = document.getElementsByClassName("fa-bell")[0];
+            let notificationDiv = document.getElementById("notification_bell");
+            let notificationNumber = document.getElementById("notification_number");
+            let notificationDropdown = document.getElementById("notification_dropdown");
+            let notificationList = [notificationDiv, notificationDropdown, notificationBell, notificationNumber, notificationSign];
+            for (let i = 0; i < notificationList.length; i++) {
+                if (notificationList[i] === target) {
+                    break;
+                }
+                if (i === notificationList.length - 1) {
+                        notificationDropdown.style.opacity = "0";
+                        notificationDropdown.style.top = "30px";
+                    const timer = ms => new Promise(resolve => setTimeout(resolve, ms));
+                    timer(200).then(() => {
+                        notificationDropdown.style.display = "none";
+                    })
+                }
+            }
+        })
     }
 
 };
